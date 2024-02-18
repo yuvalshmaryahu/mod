936dc83bbdfdcf52a253f827b819efef755df3d3
Yuval Shmaryahu <104519400+yuvalshmaryahu@users.noreply.github.com>
2024-02-18 19:08:08 +0200

    Update message_reader.c

--- message_reader.c
+++ message_reader.c
@@ -6,43 +6,56 @@
 #include <unistd.h>
 
 #include "message_slot.h"
+/*
+gcc -O3 -Wall -std=c11 -o message_reader message_reader.c 
+gcc -O3 -Wall -std=c11 -o message_sender message_sender.c
+make
+sudo insmod message_slot.ko
+sudo mknod /dev/slot0 c 235 0
+sudo chmod 666 /dev/slot0 
+./message_sender /dev/slot0 1 "Hello world"
+./message_reader /dev/slot0 1
+./message_sender /dev/slot0 1 "Dores"
+./message_reader /dev/slot0 1
+./message_sender /dev/slot0 3 "another channel"
+./message_reader /dev/slot0 3
+*/
 
 int main(int argc, char* argv[]){
     if(!(argc == 3)){
-        
-        perror("Invalid Number Of Arguments for Reader");
+        perror("Error Reading - Too many arguments");
         exit(1);
     }
 
-    char* path = argv[1];
+    char* path_to_file = argv[1];
     int channel = atoi(argv[2]);
-    int file_descriptor;
+    int fd;
     char buff[BUF_LEN];
-    int message_len;
+    int msg_len;
 
-    file_descriptor = open(path, O_RDONLY); /*open file*/
-    if(file_descriptor < 0){/*failed to open file*/
-        perror("Reader Failed To Open File");
+    fd = open(path_to_file, O_RDONLY); 
+    if(fd < 0){
+        perror("Error Reading - file not opened");
         exit(1);
     }
 
-    if(ioctl(file_descriptor, MSG_SLOT_CHANNEL, channel) < 0){ /*try ioctl*/
-        perror("Reader Failed Using ioctl"); /*ioctl failed*/
+    if(ioctl(fd, MSG_SLOT_CHANNEL, channel) < 0){ 
+        perror("Error Reading - IOCTL"); 
         exit(1);
     }
 
-    if((message_len = read(file_descriptor, buff, BUF_LEN)) < 0){/*try reading*/
-        perror("Reader Failed To Read From File");
+    if((msg_len = read(fd, buff, BUF_LEN)) < 0){
+        perror("Error Reading - reading failed");
         exit(1);
     }
 
-    if(close(file_descriptor) < 0){/*try to close*/
-        perror("Reader Failed To Close File");
+    if(close(fd) < 0){
+        perror("Error Reading - cannot close file");
         exit(1);
     }
 
-    if(write(STDOUT_FILENO, buff, message_len) < 0){ /*try print to stdout*/
-        perror("Reader Failed Printing To STDOUT");
+    if(write(STDOUT_FILENO, buff, msg_len) < 0){ 
+        perror("Error Reading - printing message not succesfull");
         exit(1);
     }
 
