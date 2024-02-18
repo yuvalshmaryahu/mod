287cc72112afa1d7a126f32dd14439af7ea31030
Yuval Shmaryahu <104519400+yuvalshmaryahu@users.noreply.github.com>
2024-02-18 19:09:13 +0200

    Update message_slot.c

--- message_slot.c
+++ message_slot.c
@@ -14,20 +14,20 @@ MODULE_LICENSE("GPL");
 
 #include "message_slot.h"
 
-message_slot_file* search_message_slot(unsigned int minor);
-void insert_message_slot(message_slot_file* msg_slot);
-void delete_message_slot(message_slot_file* msg_slot);
-channel* search_channel(unsigned long num, message_slot_file* msg_slot);
-void insert_channel(message_slot_file* msg_slot, channel* chan);
-void delete_channels(message_slot_file* msg_slot);
-void buff_to_chan(ssize_t len, const char* buff, channel* chan);
-static message_slot_file* head;
+void freeall(void);
+slot_file* search_slot(unsigned int minor);
+void delete_slot(slot_file* slot);
+channel* search_channel(unsigned long num, slot_file* slot);
+void insert_channel(slot_file* slot, channel* chan);
+void delete_channels(slot_file* slot);
+void copy_buffer_to_channel(ssize_t len, const char* buff, channel* chan);
+static slot_file* head;
 
 
 
-//searches and return message slot struct, null if not found
-message_slot_file* search_message_slot(unsigned int minor){
-    message_slot_file* curr = head;
+//searches and return slot , null if not found
+slot_file* search_slot(unsigned int minor){
+    slot_file* curr = head;
     while(!(curr == NULL)){
         if((curr->minor) == minor){
             return curr;
@@ -37,42 +37,26 @@ message_slot_file* search_message_slot(unsigned int minor){
     return NULL;
 }
 
-
-//insert messages slots to the linked list
-void insert_message_slot(message_slot_file* msg_slot){
-    message_slot_file* curr_head = head;
-    msg_slot->next = curr_head;
-    head = msg_slot;
-    return;
-}
-
-//delete message slot and its channels, knowing a message slot with this minor exists
-void delete_message_slot(message_slot_file* msg_slot){
-    delete_channels(msg_slot);
-    kfree(msg_slot);
-    return;
-}
-
-channel* search_channel(unsigned long num, message_slot_file* msg_slot){/*returns a channel struct, NULL if not initialized yet*/
-    channel* curr = msg_slot->channels;
+void freeall(void){
+    slot_file* curr = head;
+    slot_file* next = NULL;
     while(!(curr == NULL)){
-        if((curr->channel_num) == num){
-            return curr;
-        }
-        curr = curr->next;
+        /*delete all open slots*/
+        next = curr->next;
+        delete_slot(curr);
+        curr = next;
     }
-    return NULL;
 }
 
-void insert_channel(message_slot_file* msg_slot, channel* chan){/*insert a new channel to channels of msg_slot*/
-    channel* curr_head = msg_slot->channels;
-    chan->next = curr_head;
-    msg_slot->channels = chan;
+//delete message slot and all of its channels
+void delete_slot(slot_file* slot){
+    delete_channels(slot);
+    kfree(slot);
     return;
 }
-
-void delete_channels(message_slot_file* msg_slot){
-    channel* chan = msg_slot->channels;
+//delete channels of a slot
+void delete_channels(slot_file* slot){
+    channel* chan = slot->channels;
     channel* next = NULL;
     while(!(chan == NULL)){/*free all open channels*/
         next = chan->next;
@@ -82,11 +66,31 @@ void delete_channels(message_slot_file* msg_slot){
     }
     return;
 }
+//seach channel
+channel* search_channel(unsigned long num, slot_file* slot){/*returns a channel struct, NULL if not initialized yet*/
+    channel* curr = slot->channels;
+    while(!(curr == NULL)){
+        if((curr->channel_num) == num){
+            return curr;
+        }
+        curr = curr->next;
+    }
+    return NULL;
+}
 
-void buff_to_chan(ssize_t len, const char* buff, channel* chan){
+//insert channel
+void insert_channel(slot_file* slot, channel* chan){/*insert a new channel to channels of slot*/
+    channel* curr_head = slot->channels;
+    chan->next = curr_head;
+    slot->channels = chan;
+    return;
+}
+
+//copy from user's buffer to kernel's channel
+void copy_buffer_to_channel(ssize_t l, const char* buf, channel* c){
     ssize_t i;
-    for(i=0; i<len; i++){
-        (chan->data)[i] = buff[i];
+    for(i=0; i<l; i++){
+        (c->data)[i] = buf[i];
     }
 }
 
@@ -94,22 +98,27 @@ void buff_to_chan(ssize_t len, const char* buff, channel* chan){
 static int device_open( struct inode* inode,
                         struct file*  file )
 {
-    unsigned int minor = iminor(inode);
-    message_slot_file* msg_slot = search_message_slot(minor);
-
-    if(msg_slot == NULL){/*first time seeing this msg_slot so create new one*/
-        msg_slot = (message_slot_file*) kmalloc(sizeof(message_slot_file), GFP_KERNEL);
-        if (msg_slot == NULL){/*failed allocating memory*/
+    //get minor num for the node
+    unsigned int curr_minor = iminor(inode);
+    slot_file* slot = search_slot(curr_minor);
+    slot_file* curr_head ;
+    if(slot == NULL){
+        /*Create a new slot*/
+        slot = (slot_file*) kmalloc(sizeof(slot_file), GFP_KERNEL);
+        if (slot == NULL){
+            /*kmaloc failed*/
             return -ENOMEM;
         }
-        msg_slot->minor = minor;
-        msg_slot->channels = NULL;
-        msg_slot->owners = 0;
-        insert_message_slot(msg_slot);
+        /*Fixing minor num, and count of owners(next it will be increased to 1)*/
+        slot->minor = curr_minor;
+        slot->channels = NULL;
+        slot->owners = 0;
+        curr_head = head;
+        slot->next = curr_head;
+        head = slot;
     }
-
-    msg_slot->owners = msg_slot->owners + 1; /*one more owner added to the file*/
-
+    /*Increase the counter of owners in this file*/
+    slot->owners = slot->owners + 1; 
     return 0;
 }
 
@@ -119,32 +128,36 @@ static ssize_t device_write( struct file*       file,
                              size_t             length,
                              loff_t*            offset )
 {
-  char* buff;
+  char* buf;
   ssize_t i;
-  if(file->private_data == NULL || buffer == NULL){/*no open channel for this file or buffer is NULL*/
+
+  if(file->private_data == NULL || buffer == NULL){
     return -EINVAL;
   }
   
-  if(length == 0 || length > BUF_LEN){ //message lenght invalid
+  if(length == 0 || length > BUF_LEN){ 
     return -EMSGSIZE;
   }
 
-  buff = (char*) kmalloc(BUF_LEN, GFP_KERNEL);/*allocate a buffer for atomic write*/
-    if(buff == NULL){/*failed allocating memory*/
+  buf = (char*) kmalloc(BUF_LEN, GFP_KERNEL);
+    if(buf == NULL){
         return -ENOMEM;
     }
 
   for( i = 0; i < length; ++i) {
-    if(!(get_user(buff[i], &buffer[i]) == 0)){
-        kfree(buff);
+    if(!(get_user(buf[i], &buffer[i]) == 0)){
+        kfree(buf);
         return -EFAULT;
     };
   }
-  buff_to_chan(i, buff, (channel*)file->private_data); /*copy from buff to the channel*/
-  ((channel*)(file->private_data))->word_len = (unsigned long) i; /*update to length of current word of the channel*/
 
-  // free buff and return the number of input characters used
-  kfree(buff);
+  /*copy from buf to the channel*/
+  copy_buffer_to_channel(i, buf, (channel*)file->private_data); 
+  /*update to length of current word of the channel*/
+  ((channel*)(file->private_data))->word_len = (unsigned long) i; 
+
+  // free buf
+  kfree(buf);
   return i;
 }
 
@@ -157,38 +170,39 @@ static ssize_t device_read( struct file* file,
 {
     ssize_t i;
     int j;
-    unsigned long n;
-    char* word;
-    char old_message[BUF_LEN];
+    unsigned long n ;// length of waord;
+    char* text;
+    char original_message[BUF_LEN];
 
     if(((channel*)file->private_data) == NULL || buffer == NULL){/*no open channel for this file or buffer is NULL*/
         return -EINVAL;
     }
 
     n = ((channel*)(file->private_data))->word_len;
-    if(n == 0){/*no word in the channel*/
+    if(n == 0){
+        /*txt is empty*/
         return -EWOULDBLOCK;
     }
 
-    if(length < n){/*buffer is too short for the word saved in the channel*/
+    if(length < n){
+        /*buffe too short*/
         return -ENOSPC;
     }
 
-    word = ((channel*)(file->private_data))->data;
+    text = ((channel*)(file->private_data))->data;
 
-    for(i=0; i<n; i++){ /*save current buffer content to keep read atomic*/
-        if(!(get_user(old_message[i], &buffer[i]) == 0)){
-            /*as said in forum - if we failed here we will fail trying writing to the user's buffer*/
+    for(i=0; i<n; i++){ 
+        if(!(get_user(original_message[i], &buffer[i]) == 0)){
+            /*Failure reading from buffer to message*/
             return -EFAULT;
         }
     }
 
     for(i=0; i<n; ++i){
-        if(!(put_user(word[i], &buffer[i]) == 0)){ /*error while trying to write message to user buffer*/
+        if(!(put_user(text[i], &buffer[i]) == 0)){
+             /*Failue writibng to user's buffer from message*/
             for(j=0; j<i; j++){
-                /*recover original data to user's buffer,
-                as said in the forum: we know for sure put_user will work fine here*/
-                put_user(old_message[j], &buffer[j]);
+                put_user(original_message[j], &buffer[j]);
             }
             return -EFAULT;
         }
@@ -203,41 +217,45 @@ static long device_ioctl( struct   file* file,
                           unsigned long  ioctl_param )
 {
   unsigned int minor;
-  message_slot_file* msg_slot;
-  channel* chan;
+  slot_file* slot;
+  channel* ch;
 
-  // got wrong parameter - error
+
   if(!(MSG_SLOT_CHANNEL == ioctl_command_id) || ioctl_param == 0){
+    // wrong argumnets
     return -EINVAL;
   }
 
   minor = iminor(file->f_inode);
-  msg_slot = search_message_slot(minor);
-  chan = search_channel(ioctl_param, msg_slot);
+  slot = search_slot(minor);
+  ch = search_channel(ioctl_param, slot);
 
-  if(chan == NULL){ /*no channel with this number opened yet, create new one*/
-    chan = (channel*) kmalloc(sizeof(channel), GFP_KERNEL);
-    if(chan == NULL){/*error when allocating memory*/
+  if(ch == NULL){ 
+    /*create new channel*/
+    ch = (channel*) kmalloc(sizeof(channel), GFP_KERNEL);
+    if(ch == NULL){
+        /*Allocating memory Failure*/
         return -ENOMEM;
     }
 
-    chan->channel_num = ioctl_param;
-    chan->data = (char*) kmalloc(BUF_LEN, GFP_KERNEL);
-    if(chan->data == NULL){/*failed allocating memory*/
-        kfree(chan);
+    ch->channel_num = ioctl_param;
+    ch->data = (char*) kmalloc(BUF_LEN, GFP_KERNEL);
+    if(ch->data == NULL){
+        /*Allocating memory Failure*/
+        kfree(ch);
         return -ENOMEM;
     }
-    chan->word_len = 0;
-    insert_channel(msg_slot, chan);
+    ch->word_len = 0;
+    insert_channel(slot, ch);
   }
-
-  file->private_data = chan; /*set the channel to the message slot file*/
+  /*set the channel to the message slot file*/
+  file->private_data = ch; 
   
   return 0;
 }
 
 
-//==================== DEVICE SETUP ============================= /*code skeleton taken from 6th recitation and modified*/
+//==================== DEVICE SETUP ============================= //
 
 // The File Operations
 struct file_operations Fops = {
@@ -248,8 +266,7 @@ struct file_operations Fops = {
   .unlocked_ioctl = device_ioctl,
 };
 
-//---------------------------------------------------------------
-// Initialize module
+
 static int __init simple_init(void)
 {
   int rc = -1;
@@ -266,18 +283,12 @@ static int __init simple_init(void)
   return 0;
 }
 
-//---------------------------------------------------------------
+
 static void __exit simple_cleanup(void)
 {
-    message_slot_file* curr = head;
-    message_slot_file* next = NULL;
-    while(!(curr == NULL)){/*delete all open message slots*/
-        next = curr->next;
-        delete_message_slot(curr);
-        curr = next;
-    }
-
-    // Should always succeed
+    //free the linkedlist
+    freeall();
+    //unregister as seen in the recitation
     unregister_chrdev(MAJOR_NUM, DEVICE_NAME);
 }
 
