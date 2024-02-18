fa72b9ad5d82a723b8fcadccd8d4d3ba4977030a
Yuval Shmaryahu <104519400+yuvalshmaryahu@users.noreply.github.com>
2024-02-18 19:19:51 +0200

    Update message_slot.h

--- message_slot.h
+++ message_slot.h
@@ -10,20 +10,4 @@
 
 #define DEVICE_NAME "message_slot_device"
 
-typedef struct slot_file
-{
-    unsigned int minor;
-    unsigned long owners;
-    struct slot_file* next;  
-    struct channel* channels;    
-} slot_file;
-
-typedef struct channel
-{
-    unsigned long channel_num;
-    unsigned long word_len;
-    char* data;
-    struct channel* next;
-} channel;
-
 #endif
